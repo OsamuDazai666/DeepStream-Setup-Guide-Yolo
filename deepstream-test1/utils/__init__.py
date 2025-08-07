247cacb07ca9f023a5c994b8dcee0c1ad5c7e8ce
OsamuDazai666 <rajhasham1@gmail.com>
2025-08-07 11:41:16 +0000

    changed common directory to utils

---
+++ deepstream-test1/utils/__init__.py
@@ -0,0 +1,10 @@
+from utils.bus_call import bus_call_func
+from utils.fps import GetFPS, PerfData
+from utils.platform_info import PlatformInfo
+from utils.tools import long_to_uint64
+
+__all__ = [
+    "GetFPS", 
+    "PerfData",
+    "PlatformInfo",
+]
